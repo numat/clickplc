1a898cbd92bd32455559c81c9a0eb053f2366123
JamesJeffryes <jamesgjeffryes@gmail.com>
2021-09-06 09:38:29 -0500

    Trim extra coils from _get_c response

--- clickplc/driver.py
+++ clickplc/driver.py
@@ -268,7 +268,7 @@ class ClickPLC(AsyncioModbusClient):
         coils = await self.read_coils(start_coil, count)
         if count == 1:
             return coils.bits[0]
-        return {f'c{(start + i)}': bit for i, bit in enumerate(coils.bits)}
+        return {f'c{(start + i)}': bit for i, bit in enumerate(coils.bits) if i < count}
 
     async def _get_df(self, start: int, end: int) -> Union[dict, float]:
         """Read DF registers. Called by `get`.
--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open('README.md', 'r') as in_file:
 
 setup(
     name='clickplc',
-    version='0.3.4',
+    version='0.3.5',
     description="Python driver for Koyo Ethernet ClickPLCs.",
     long_description=long_description,
     long_description_content_type='text/markdown',
